e9ce8aabdcc01f56a8f18753a39ba68d0b37474d
Joseph Egglestone <Joseph Egglestone>
2016-11-10 20:42:45 +0000

    Added libraries and NuGet packages

---
+++ Shuffler.Tests/DocumentFormatterTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Microsoft.Office.Interop.Word;
+
+namespace Shuffler.Tests
+{
+    [TestFixture]
+    public class DocumentFormatterTests
+    {
+        [Test]
+        public void ShuffleClauserUnits_MovesSomeThings()
+        {
+            var sentence = new Range();
+            
+        }
+    }
+}
