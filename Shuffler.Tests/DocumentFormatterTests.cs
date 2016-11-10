2d92c394fcd82de1b30159d30995d44d730834b5
Joseph Egglestone <Joseph Egglestone>
2016-11-10 23:55:28 +0000

    Added test tools

--- Shuffler.Tests/DocumentFormatterTests.cs
+++ Shuffler.Tests/DocumentFormatterTests.cs
@@ -1,5 +1,8 @@
 ﻿using NUnit.Framework;
 using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;
+using Rhino.Mocks;
+using CSWordRemoveBlankPage.Services;
 
 namespace Shuffler.Tests
 {
@@ -7,10 +10,18 @@ namespace Shuffler.Tests
     public class DocumentFormatterTests
     {
         [Test]
-        public void ShuffleClauserUnits_MovesSomeThings()
+        public void ShuffleClauserUnits_ToTheBeginningOfTheSentence()
         {
-            var sentence = new Range();
+            // arrange
+            var sentence = MockRepository.GenerateMock<Range>();
+            sentence.Text = "PRENThe meeting VBwas over CSbefore he VBhad a chance VBto speak BKP.";
             
+            // act
+            var documentFormatter = new DocumentFormatter();
+            documentFormatter.ShuffleClauserUnits(sentence);
+
+            // assert
+            Assert.That(sentence.Text, Is.EqualTo("CSbefore he VBhad a chance VBto speak BKP, PRENThe meeting VBwas over BKP."));
         }
     }
 }
